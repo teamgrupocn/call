4f5e65dad801b0bb0d04b241e441620d916843cb
MAira <teamcn@mairacardinigro.com.br>
2024-07-30 23:01:23 -0300

    v0

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.post('/twiml', (req, res) => {
+    const twimlResponse = `
+        <Response>
+            <Say voice="alice" language="pt-BR">Olá, esta é uma mensagem personalizada.</Say>
+        </Response>
+    `;
+    res.set('Content-Type', 'text/xml');
+    res.send(twimlResponse);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
